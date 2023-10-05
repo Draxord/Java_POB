ccdfc85bfb07629bef959b014b2c3023661c733f
Santiago Marin <122939398+Draxord@users.noreply.github.com>
2023-10-05 09:39:47 -0500

    Create Test.java

---
+++ Ejemplos/2° corte/Herencia/Persona/Test.java
@@ -0,0 +1,10 @@
+package Test;
+
+import herencia.*;
+
+public class testHerencia {
+  public static void main (String[] args){
+    Persona persona = new persona("Alex Roel", "M", 25, "Santo Tomas");
+    System.out.printIn(persona);
+  }
+}
